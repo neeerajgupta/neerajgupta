5b250e4845956033be7d8e21eb00d18468770450
Neeraj Gupta <soft.guptaneeraj@gmail.com>
2024-12-29 11:23:45 +0530

    second changes scss

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'neerajgupta' title`, () => {
+  it(`should have the 'NeerajGupta' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('neerajgupta');
+    expect(app.title).toEqual('NeerajGupta');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, neerajgupta');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, NeerajGupta');
   });
 });
