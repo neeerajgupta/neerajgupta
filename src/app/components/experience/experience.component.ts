04458ee40f1b3ef69db4b80351d832efe184f752
Neeraj Gupta <soft.guptaneeraj@gmail.com>
2025-01-12 00:51:04 +0530

    public education

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { TimelineModule } from 'primeng/timeline';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [TimelineModule,CardModule,ButtonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements OnInit {
+  events: any;
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1000, 
+    });
 
+    this.events = [
+      { status: 'CRISIL Limited', date: '11/11/2023 ', icon: '../../../assets/IMAGE/experience-removebg-preview.png', image: 'crisil.png' },
+      { status: 'Comming Soon', date: '', icon: '../../../assets/IMAGE/commingsoon.jpeg',image: 'commingsoon.jpeg'},
+  ];
+  }
 }
