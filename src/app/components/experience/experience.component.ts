fb099c819ca77526cb56a90d9ab4e5e1179575dc
Neeraj Gupta <neerajgupta@Neerajs-MacBook-Air.local>
2026-05-01 21:56:39 +0530

    date changes

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import AOS from 'aos';
-import 'aos/dist/aos.css';
+// import 'aos/dist/aos.css';
 import { TimelineModule } from 'primeng/timeline';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
@@ -20,7 +20,7 @@ export class ExperienceComponent implements OnInit {
 
     this.events = [
       { status: 'Russell Tobin', date: '11/11/2023 ', enddate:'31/03/2026', icon: '../../../assets/IMAGE/experience-removebg-preview.png', image: 'russelltobin.svg' },
-      { status: 'Quess Corp', date: '11/11/2023 ', enddate:'Present', icon: 'https://www.quesscorp.com/wp-content/uploads/2022/11/quessbluesvg.svg', image: 'quessbluesvg.svg' },
+      { status: 'Quess Corp', date: '01/04/2026 ', enddate:'Present', icon: 'https://www.quesscorp.com/wp-content/uploads/2022/11/quessbluesvg.svg', image: 'quessbluesvg.svg' },
       
       { status: 'Comming Soon', date: '', enddate:'', icon: '../../../assets/IMAGE/commingsoon.jpeg',image: 'commingsoon.jpeg'},
   ];
